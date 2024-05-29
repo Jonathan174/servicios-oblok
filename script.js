77e73617c8355be4c0d72b401c7c8450d4b03472
Jonathan174 <64048657+Jonathan174@users.noreply.github.com>
2024-05-29 16:50:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-// Select relevant HTML elements
-const filterSelect = document.querySelector("#filter-select");
-const filterableCards = document.querySelectorAll("#filterable-cards .card");
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
 
-// Function to filter cards based on filter selection
-const filterCards = (e) => {
-    const filterValue = e.target.value;
-
-    filterableCards.forEach(card => {
-        if(card.dataset.name === filterValue || filterValue === "all") {
-            return card.classList.replace("hide", "show");
-        }
-        card.classList.add("hide");
+document.querySelectorAll('.products-container .product').forEach(product =>{
+  product.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
     });
-}
+  };
+});
 
-// Add event listener to the select element
-filterSelect.addEventListener("change", filterCards);
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
